964bc81956695c3ae7582d2e944cc11d78c1ebdb
Nikhil John <nikhiljohn1010@gmail.com>
2022-12-21 15:59:14 +0530

    updated tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "instance_ami" {
 output "instance_arn" {
   value = aws_instance.blog.arn
 }
+
+output "blog_url" {
+  value = aws_instance.blog.public_dns
+}
+
+output "blog_ip" {
+  value = aws_instance.blog.public_ip
+}
