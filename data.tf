d029b1b991c14d6b75783ff5a7f61141970484e9
Nikhil John <nikhiljohn1010@gmail.com>
2022-12-21 12:53:38 +0530

    Created security group

---
+++ data.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
