f8ebc542c300d9dc45f8d50a4039515c5905476f
Nikhil John <nikhiljohn1010@gmail.com>
2022-12-21 13:14:20 +0530

    used module for sg

--- main.tf
+++ main.tf
@@ -2,36 +2,9 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
   }
-}
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http, https in. Allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type          = "ingress"
-  from_port     = 80
-  to_port       = 80
-  protocol      = "tcp"
-  cidr_blocks   = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_all_out" {
-  type          = "egress"
-  from_port     = 0
-  to_port       = 0
-  protocol      = "-1"
-  cidr_blocks   = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
 }
