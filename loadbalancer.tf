e0365c8fbc2607a197feac402482bcc57d59c234
Nikhil John <nikhiljohn1010@gmail.com>
2022-12-21 18:04:49 +0530

    autoscale

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -43,8 +43,8 @@ module "autoscaling" {
   version   = "6.7.0"
   
   name      = "blog"
-  min-size  = 1
-  max-size  = 2
+  min_size  = 1
+  max_size  = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
