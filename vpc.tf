c582e87d04422df64e1cc1f7ccb2598bc5cc492e
Nikhil John <nikhiljohn1010@gmail.com>
2022-12-21 17:09:44 +0530

    remove subnet

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
